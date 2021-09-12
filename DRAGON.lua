bb485b3c36086e53fabb7d705daca0a2384c7ff5
devsasabody1 <85717562+mostafanotbody@users.noreply.github.com>
2021-09-13 00:33:24 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -168,7 +168,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/mostafanotbody/snow/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1316,7 +1316,7 @@ echo '💞 ═───═⟦𝚂𝙽𝙾𝚆⟧═───═💞 \n ❤↝ م
 end
 if text == 'تحديث السورس ' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/snow/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,'💞  تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -2985,9 +2985,9 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/library')
-os.execute('wget https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/snow/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/snow/main/library')
+os.execute('wget https://raw.githubusercontent.com/mostafanotbody/snow/main/File_Bot')
 send(msg.chat_id_, msg.id_,' 💞  تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -5902,7 +5902,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/mostafanotbody/snow/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -5940,7 +5940,7 @@ t = " ❤ الملف  ⇦↯"..file.."\n ❤ تم تعطيل ملف \n"
 else
 t = " ❤ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/snow/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -5960,7 +5960,7 @@ t = " ❤ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ❤ الملف  ⇦↯"..file.."\n ❤ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/hogan9/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/mostafanotbody/snow/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
